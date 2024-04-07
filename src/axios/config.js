1bd81a1763d76b69436b3745b8c36e0f112c5de2
NegativeLearning <PlayBi2002@gmail.com>
2024-04-08 00:14:50 +0700

    update config axios

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://api.banghehoamai.com/api/",
   //   timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
